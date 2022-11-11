2f97b93cc8a3d28752e6adfb02819bcbb4aa9774
bkhorvat <bkhorvat.php@gmail.com>
2022-11-11 14:08:06 +0100

    Add first validation form add

--- app/add task.php
+++ app/add task.php
@@ -1,20 +1,37 @@
 <?php
-  $title = $_POST['title'];
-  $description = $_POST['description'];
-  $dateStart = $_POST['dateStart'].' '.$_POST['timeStart'];
-  $dateEnd = $_POST['dateEnd'].' '.$_POST['timeEnd'];
-  $user_id = $_COOKIE['id'];
 
-  if($title == ''){
+if ($_POST['title'] == '') {
     echo 'Введите задание';
     exit();
-  }
+}
+if ($_POST['dateStart'] == '') {
+    echo 'Введите дату начала';
+    exit();
+}
+if($_POST['timeStart'] == ''){
+    echo 'Введите времья начала';
+    exit();
+}
+if ($_POST['dateEnd'] == '') {
+    echo 'Введите дату конца';
+    exit();
+}
+if($_POST['timeEnd'] == ''){
+    echo 'Введите времья конца';
+    exit();
+}
 
-  require $_SERVER['DOCUMENT_ROOT'] . '/database/PDO config DB.php';
+$title = $_POST['title'];
+$description = $_POST['description'];
+$dateStart = $_POST['dateStart'] . ' ' . $_POST['timeStart'];
+$dateEnd = $_POST['dateEnd'] . ' ' . $_POST['timeEnd'];
+$user_id = $_COOKIE['id'];
 
-  $sql = 'INSERT INTO tasks(title, description, dateStart, dateEnd, user_id) VALUES(:title, :description, :dateStart, :dateEnd, :user_id)';
-  $query = $pdo->prepare($sql);
-  $query->execute(['title' => $title, 'description' => $description, 'dateStart' => $dateStart, 'dateEnd' => $dateEnd, 'user_id' => $user_id]);
+require $_SERVER['DOCUMENT_ROOT'] . '/database/PDO config DB.php';
 
-  header('Location: /');
- ?>
+$sql = 'INSERT INTO tasks(title, description, dateStart, dateEnd, user_id) VALUES(:title, :description, :dateStart, :dateEnd, :user_id)';
+$query = $pdo->prepare($sql);
+$query->execute(['title' => $title, 'description' => $description, 'dateStart' => $dateStart, 'dateEnd' => $dateEnd, 'user_id' => $user_id]);
+
+header('Location: /');
+?>
